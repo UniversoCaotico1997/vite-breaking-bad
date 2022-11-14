563af77b2ff9c0fed94e8e3bb34263d9fbb19da5
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-14 23:18:16 +0100

    Creazione store.js + importazione dei dati

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,50 @@
 <script>
+// import AppHeader from "./components/AppHeader.vue";
+import { store } from "./assets/js/store";
 import axios from "axios";
 export default {
   name: `App`,
+  components: {
+    // AppHeader
+  },
   data() {
     return {
-      API_URL: `https://www.breakingbadapi.com/api/characters`,
-      name: null,
-      status: null,
-      img: null
-
-
+      store,
     }
   },
   methods: {
     callApi(url) {
       axios.get(url).then(response => {
         console.log(response);
+        this.store.characters = response.data
+        console.log(this.store.characters);
       })
-        .catch(err => {
-          console.error(err.message);
-        })
     }
 
   },
   mounted() {
-
+    this.callApi(this.store.API_URL)
   }
 }
 </script>
 
 <template>
 
+  <h1>Breaking bad Api</h1>
+  <div class="container">
+    <div class="characters"></div>
+    <div class="row row-cols-5">
+      <div class="col" v-for="item in store.characters">
+        <div class="card">
+          <img :src="item.img" class="card-img-top" alt="">
+          <div class="card-body">
+            <h5 class="card-title">{{ item.name }} </h5>
+            <p class="card-text">{{ item.status }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
---
+++ src/assets/js/store.js
@@ -0,0 +1,8 @@
+import { reactive } from "vue";
+export const store = reactive({
+    API_URL: `https://www.breakingbadapi.com/api/characters`,
+    characters: null,
+    img: null,
+    name: null,
+    status: null
+})
